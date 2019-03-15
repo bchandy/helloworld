8f940d3ab1f4d63d8eb2508ac5800d92b9ba8f44
bchandy <bchandy@gmail.com>
2019-03-14 21:49:03 -0700

    Update SpringdemoApplication.java

--- com/example/symantec/springdemo/SpringdemoApplication.java
+++ com/example/symantec/springdemo/SpringdemoApplication.java
@@ -1,13 +1,8 @@
-package com.example.symantec.springdemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdemoApplication.class, args);
+		System.out.println("Hello World application");
 	}
 
 }
